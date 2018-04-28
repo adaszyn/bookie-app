decbabe6fa6bfb7f4b43e3a250ef05d0fdca08bd
ranjini <ranjini1992@gmail.com>
2018-04-28 17:27:16 +0200

    add a toggle between list and grid

--- src/components/home/HomeView.js
+++ src/components/home/HomeView.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { observer, inject } from "mobx-react";
-import { Grid, Header , Divider, List} from "semantic-ui-react";
+import { Grid, Header , Divider, Icon, Menu, List} from "semantic-ui-react";
 import "./HomeView.css";
 import { BookCard } from "../book-card/BookCard";
 import { NoteCard, NoteList } from "../note-card/NoteCard";
@@ -10,33 +10,48 @@ import { Carousel } from "../carousel/Carousel";
 
 @observer
 export class HomeView extends Component {
+  state = {
+    activeItem: "th-btn"
+  }
   componentDidMount() {
     this.props.getAllNotes();
   }
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render() {
     return (
       <div>
         <Header as="h1">Recent Notes</Header>
         <Divider />
-        <Carousel
-        style={{minHeight: "280px"}}
-          items={this.props.notes}
-          renderItem={note => (
-            <Link to={"/notes/" + note.id} key={note.id}>
-              <NoteCard
-                key={note.id}
-                title={note.bookId}
-                isFav={note.isFav}
-                meta={note.dateModified}
-                description={note.content}
-              />
-            </Link>
-          )}
-          itemKey={"id"}
-          perPage={3}
-        />
-        <List style={{maxHeight: "280px", overflow: "scroll"}}>
-          {this.props.notes.map(note => (
+        <Menu>
+          <Menu.Item name='th-btn' active={this.state.activeItem === 'th-btn'} onClick={this.handleItemClick}>
+            <Icon name = 'th'/>Grid
+          </Menu.Item>
+          <Menu.Item name='list-btn' active={this.state.activeItem === 'list-btn'} onClick={this.handleItemClick}>
+            <Icon name = 'list'/>List
+          </Menu.Item>
+        </Menu>
+        {this.state.activeItem === 'th-btn' ? 
+          <Carousel
+            style={{minHeight: "280px", display:"block"}}
+            items={this.props.notes}
+            renderItem={note => (
+              <Link to={"/notes/" + note.id} key={note.id}>
+                <NoteCard
+                  key={note.id}
+                  title={note.bookId}
+                  isFav={note.isFav}
+                  meta={note.dateModified}
+                  description={note.content}
+                />
+              </Link>
+            )}
+            itemKey={"id"}
+            perPage={3}
+          /> : 
+          <List
+            style={{maxHeight: "280px", display:"block", overflow: "scroll"}}>
+            {this.props.notes.map(note => (
             <Link to={"/notes/" + note.id} key={note.id}>
               <NoteList
                 key={note.id}
@@ -45,7 +60,9 @@ export class HomeView extends Component {
                 meta={note.dateModified}
               />
             </Link>
-        ))}</List>
+          ))}/>
+          </List> 
+        }
         <Divider />
         <Header as="h1">All Books</Header>
         <Grid>
