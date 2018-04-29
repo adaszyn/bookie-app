6b28dd80aa5896009aea7ddf0b0d4611fdd6ae7a
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2018-04-29 18:23:03 +0200

    BreadCrumbs for create note view

--- src/components/note-create/NoteCreateView.js
+++ src/components/note-create/NoteCreateView.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { observer, inject } from "mobx-react";
-import { Button } from "semantic-ui-react";
+import {Breadcrumb, Button} from "semantic-ui-react";
 import RichTextEditor from "react-rte";
+import {Link} from "react-router-dom";
 
 @observer
 export class NoteCreateView extends Component {
@@ -20,8 +21,21 @@ export class NoteCreateView extends Component {
       .then(() => this.props.history.push(`/books/${bookId}/`));
   };
   render() {
-    return (
+      const bookId = this.props.match.params.id;
+      const book = this.props.books.get(bookId);
+      return (
       <div>
+          <Breadcrumb>
+              <Breadcrumb.Section><Link to="/">Home</Link></Breadcrumb.Section>
+              <Breadcrumb.Divider> > </Breadcrumb.Divider>
+              <Breadcrumb.Section>
+                  <Link to={"/books/" + bookId}>
+                      {book.title}
+                  </Link>
+              </Breadcrumb.Section>
+              <Breadcrumb.Divider> > </Breadcrumb.Divider>
+              <div className="active section">Create new note</div>
+          </Breadcrumb>
         <RichTextEditor value={this.state.note} onChange={this.onNoteChange} />
         <Button onClick={this.onSubmit}>Save</Button>
       </div>
@@ -30,6 +44,7 @@ export class NoteCreateView extends Component {
 }
 export const NoteCreateViewContainer = inject(stores => {
   return {
-    saveNote: stores.notesStore.saveNote
+    saveNote: stores.notesStore.saveNote,
+    books: stores.booksStore.books,
   };
 })(NoteCreateView);
