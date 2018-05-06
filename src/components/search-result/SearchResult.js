2836c9e870fe026e2efb3cfbf99d1e706e6a06fd
ranjini <ranjini1992@gmail.com>
2018-05-06 15:27:03 +0200

    should have used isbn instead of id

--- src/components/search-result/SearchResult.js
+++ src/components/search-result/SearchResult.js
@@ -2,12 +2,12 @@ import React from "react";
 import {Item, Rating} from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
-export const SearchResult = ({ image, description, title, rating, id }) => {
+export const SearchResult = ({ image, description, title, rating, id, isbn10 }) => {
   return (
-    <Item key={id} as={Link} to={'/books/'+id}>
+    <Item key={id} as={Link} to={'/books/'+isbn10}>
       <Item.Image size='tiny' src={image}/>
       <Item.Content>
-        <Item.Header as='a'>{title}</Item.Header>
+        <Item.Header>{title}</Item.Header>
         <Item.Meta>{rating}</Item.Meta>
         <Item.Description>
           <Rating disabled maxRating="5" rating={rating} /> {rating}
