ed4a9c8bf121172a4161b5d917f45ffd04985ef5
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2018-05-17 14:36:59 +0200

    SearchBar displayed only on desktop

--- src/components/dashboard/DashboardWrapper.js
+++ src/components/dashboard/DashboardWrapper.js
@@ -4,31 +4,39 @@ import { Container, Image, Menu } from "semantic-ui-react";
 import { BookSearchContainer } from "../book-search/BookSearch";
 import logo from "../../assets/logo_white.svg";
 import "./DashboardWrapper.css";
+import Responsive from "semantic-ui-react/dist/es/addons/Responsive/Responsive";
 
 export class DashboardWrapper extends Component {
   render() {
     return (
       <div className="App">
+        <Container className="navbar">
         <Menu fixed="top" inverted size="large">
-          <Container className="navbar">
             <Menu.Item header>
               <Image as={Link} to="/" src={logo} className="logo" />
             </Menu.Item>
-            <BookSearchContainer />
+            <Menu.Item>
+              <Responsive minWidth={1000}>
+                <BookSearchContainer />
+              </Responsive>
+            </Menu.Item>
             <Menu.Item
               position="right"
               name="All Notes"
               as={Link}
               to="/notes"
             />
+            <Responsive maxWidth={1000} as="a" className="right item" href="/search">
+              Search
+            </Responsive>
             <Menu.Item position="right" name="About" as={Link} to="/about" />
             <Menu.Item
               position="right"
               name="logout"
               onClick={this.props.logOut}
             />
-          </Container>
         </Menu>
+        </Container>
 
         <Container text style={{ marginTop: "7em" }}>
           {this.props.children}
